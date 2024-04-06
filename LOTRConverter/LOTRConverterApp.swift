c4835a15360e435c58f01f6da45fd25c8e694afa
Sebastian Cioata <sebastiancioata98@gmail.com>
2024-04-06 11:21:15 +0300

    first commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Sebastian Cioată on 06.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

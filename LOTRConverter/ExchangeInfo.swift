47ff463cb059c0a6d69622484632266cd9dd39d2
Sebastian Cioata <sebastiancioata98@gmail.com>
2024-04-06 16:52:28 +0300

    Info Screen

---
+++ LOTRConverter/ExchangeInfo.swift
@@ -0,0 +1,57 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter
+//
+//  Created by Sebastian Cioată on 06.04.2024.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        ZStack{
+            // Background parchment imange
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack{
+                // Title text
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(3)
+                
+                // Description text
+                Text ("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title2)
+                    .padding()
+                // Exchange rats
+                ExchangeRate(leftImage: .goldpiece, text: "! Gold Piece = 4 Gold Pennies", rightImage: .goldpenny)
+                
+                ExchangeRate(leftImage: .goldpenny, text: "1 Gold Penny = 4 Silver Pieces", rightImage: .silverpiece)
+                
+                ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pennies", rightImage: .silverpenny)
+                
+                ExchangeRate(leftImage: .silverpenny, text: "1 Silver Penny = 100 Copper Pennies", rightImage: .copperpenny)
+                
+                // Done button
+                Button("Done"){
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .foregroundStyle(.black)
+        }
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
+
+
---
+++ LOTRConverter/ExchangeRate.swift
@@ -0,0 +1,36 @@
+//
+//  ExchangeRate.swift
+//  LOTRConverter
+//
+//  Created by Sebastian Cioată on 06.04.2024.
+//
+
+import SwiftUI
+
+struct ExchangeRate: View {
+    let leftImage: ImageResource
+    let text: String
+    let rightImage: ImageResource
+    
+    var body: some View {
+        HStack{
+            // Left currency image
+            Image(leftImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            
+            // Exchange rate text
+            Text(text)
+            
+            // Right currency image
+            Image(rightImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+        }
+    }
+}
+#Preview {
+    ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pennies", rightImage: .silverpenny)
+}

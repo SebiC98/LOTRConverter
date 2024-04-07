7dc2d22805a8af9e9f309ccfe496b6a53f7d4663
Sebastian Cioata <sebastiancioata98@gmail.com>
2024-04-07 13:27:28 +0300

    Finished app

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import TipKit
 
 struct ContentView: View {
     @State var showExchangeInfo = false
@@ -59,6 +60,7 @@ struct ContentView: View {
                         .onTapGesture {
                             showSelectCurrency.toggle()
                         }
+                        .popoverTip(CurrencyTip(), arrowEdge: .bottom)
                         // Textfield
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
@@ -118,6 +120,9 @@ struct ContentView: View {
                 
             }
             //            .border(.blue)
+        }
+        .task {
+            try? Tips.configure()
         }
         .onChange(of: leftAmount) {
             if leftTyping{
